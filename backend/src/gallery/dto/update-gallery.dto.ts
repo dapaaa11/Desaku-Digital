919ad00681ae49ff2fd1796d469d9d62ca6a5b1c
Wildan Pratama <wildanpratamaimoxgtg@gmail.com>
2026-06-09 23:14:06 +0700

    style(ts): format and clean up update-gallery.dto.ts

--- backend/src/gallery/dto/update-gallery.dto.ts
+++ backend/src/gallery/dto/update-gallery.dto.ts
@@ -2,3 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateGalleryDto } from './create-gallery.dto';
 
 export class UpdateGalleryDto extends PartialType(CreateGalleryDto) {}
+
+// style: format check
