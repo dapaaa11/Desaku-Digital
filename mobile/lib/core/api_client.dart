3288040f5347d31cccdb1731769e37dcf553fd0a
Wildan Pratama <wildanpratamaimoxgtg@gmail.com>
2026-06-09 21:09:16 +0700

    style: code standard enforcement on API client and constants

--- mobile/lib/core/api_client.dart
+++ mobile/lib/core/api_client.dart
@@ -6,24 +6,28 @@ class ApiClient {
   static String? _token;
 
   static Dio get dio {
-    _dio ??= Dio(BaseOptions(
-      baseUrl: AppConstants.baseUrl,
-      connectTimeout: const Duration(seconds: 15),
-      receiveTimeout: const Duration(seconds: 15),
-    ));
+    _dio ??= Dio(
+      BaseOptions(
+        baseUrl: AppConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+      ),
+    );
 
     _dio!.interceptors.clear();
 
     // Interceptor: Otomatis menyematkan JWT token jika tersedia
-    _dio!.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        if (_token != null) {
-          options.headers['Authorization'] = 'Bearer $_token';
-        }
-        options.headers['Content-Type'] = 'application/json';
-        return handler.next(options);
-      },
-    ));
+    _dio!.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          if (_token != null) {
+            options.headers['Authorization'] = 'Bearer $_token';
+          }
+          options.headers['Content-Type'] = 'application/json';
+          return handler.next(options);
+        },
+      ),
+    );
 
     return _dio!;
   }
