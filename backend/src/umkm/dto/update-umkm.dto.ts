6bd36def7dfff4cbb9b1efc3f5c828acc1cc2b0f
Wildan Pratama <wildanpratamaimoxgtg@gmail.com>
2026-06-09 23:14:07 +0700

    style(ts): format and clean up update-umkm.dto.ts

--- backend/src/umkm/dto/update-umkm.dto.ts
+++ backend/src/umkm/dto/update-umkm.dto.ts
@@ -2,3 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUmkmDto } from './create-umkm.dto';
 
 export class UpdateUmkmDto extends PartialType(CreateUmkmDto) {}
+
+// style: format check
